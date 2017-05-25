a10386637285392dcc19de1ee0994b7ee6c93fd2
Unknown <victor.cranz@stud.h-da.de>
2017-05-25 18:08:12 +0200

    Szenario 2

--- src/szenario1/Szenario1.java
+++ src/szenario1/Szenario1.java
@@ -9,15 +9,17 @@ public class Szenario1 {
 	final static String GSUTILPATH ="/home/coxtor/.opt/google-cloud-sdk/bin/gsutil";
 	final static String[] FILENAME = {	"0Byte", "1Byte", "512Byte", "1KB", "512KB", "1MB"};//, "512MB", "1GB", "4GB" };
 	final static String BASEPATH = System.getProperty("user.dir") + "/../data/";
+	final static String ResultsFileName = "Szenario1_Groundtruth-"+ System.currentTimeMillis() +".txt";
+
 	public static void main(String[] args) throws IOException {
 	BufferedWriter output;
-	output = new BufferedWriter(new FileWriter("GroundtruthData.txt", false));
+	output = new BufferedWriter(new FileWriter(ResultsFileName, false));
 	String dataToWrite = "";
 	for(int i : FILEAMOUNT){
-		dataToWrite += "Anticipated time Ms " + i + " , "; 
+		dataToWrite += "Time Ms " + i + " , "; 
 	};	
 	
-	output.write("Filesize , Elapsed Time Ms, " + dataToWrite);
+	output.write("Filesize , " + dataToWrite);
 	output.newLine();
 	output.close();
 		System.out.println("Transfer started");
@@ -25,19 +27,18 @@ public class Szenario1 {
 			String command = GSUTILPATH +" cp " + BASEPATH + file + " gs://iot-test_hda ";
 			LinuxInteractor interactor = new LinuxInteractor();
 			String result = interactor.executeCommand(command, true);
-			//System.out.println("Command: "+ command);
-			//System.out.println("result: "+ result);
-			System.out.println("Transferred " + file + " in " +  interactor.elapsedTime + " Milliseconds");	
-			output = new BufferedWriter(new FileWriter("GroundtruthData.txt", true));
+
+			System.out.println("Transferred " + file + " in " +  interactor.elapsedTime + " Ms (" + interactor.elapsedTime/1000 +" sec.)");	
+			output = new BufferedWriter(new FileWriter(ResultsFileName, true));
 			dataToWrite = "";
 			for(int i : FILEAMOUNT){
 				dataToWrite += interactor.elapsedTime*i + " , "; 
 			};
-			output.append(file + " , " +  interactor.elapsedTime + " , " +  dataToWrite);
+			output.append(file + " , " +  dataToWrite);
 			output.newLine();
 			output.close();
 		}
-		System.out.println("Operation terminated as expected");
+		System.out.println("Operation terminated as expected and wrote to file " + ResultsFileName);
 		
 		
 		
