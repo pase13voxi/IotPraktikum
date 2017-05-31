8e9d0749ead5caf3bc5d936afc9f7f06c518a3c1
pase13voxi <pascal_bechtoldt@web.de>
2017-05-31 18:24:41 +0200

    Parallele Zugriffe können jetzt alle hintereinander automatisiert ausgeführt werden.

--- src/szenario3/Szenario3.java
+++ src/szenario3/Szenario3.java
@@ -3,81 +3,85 @@ package szenario3;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
+import java.util.Vector;
 
 import szenario1.LinuxInteractor;
 
 public class Szenario3 extends Thread {
-	final static int[] FILEAMOUNT = { 0, 1, 5,};// 10};//, 100, 1000, 10000 };
-	final static String GSUTILPATH ="/home/coxtor/.opt/google-cloud-sdk/bin/gsutil";
-	final static String[] FILENAME = {	"0Byte", "1Byte", "512Byte", "1KB", "512KB", "1MB"};//, "512MB", "1GB", "4GB" };
+	final static int[] FILEAMOUNT = { 0, 1, 2, 4, 8, 16/*, 32, 64 */};
+	final static String GSUTILPATH = "/home/coolpharaoh/.opt/google-cloud-sdk/bin/gsutil";
+	final static String[] FILENAME = { "0Byte", "1Byte"/*, "512Byte", "1KB", "512KB", "1MB", "512MB", "1GB", "4GB" */};
 	final static String BASEPATH = System.getProperty("user.dir") + "/../data/";
-	final static String ResultsFileName = "Szenario2_Realdata-Sequentiell-"+ System.currentTimeMillis() +".txt";
-	
-	
-	public Szenario3(Object parameter){
-		String command = parameter.toString();
+	final static String ResultsFileName = "Szenario3_Groundtruth-"+ System.currentTimeMillis() +".txt";
+
+	// values needed to trace the threads
+	String threadCommand = "";
+	Vector<Long> threadReturnValue = new Vector<>();
+
+	public Szenario3(String command, Vector<Long> returnValues) {
+		threadCommand = command;
+		threadReturnValue = returnValues;
 	}
-	
-	public void run(){
+
+	@Override
+	public void run() {
 		LinuxInteractor interactor = new LinuxInteractor();
-		String result = interactor.executeCommand(this.command, true);
-
+		String result = interactor.executeCommand(threadCommand, true);
+		threadReturnValue.addElement(interactor.elapsedTime);
 	}
-	
-	
+
 	public static void main(String[] args) throws IOException {
-	BufferedWriter output;
-	
-	output = new BufferedWriter(new FileWriter(ResultsFileName, false));	
-	String dataToWrite = "";
-	for(int i : FILEAMOUNT){
-		dataToWrite += "Time Ms " + i + " , "; 
-	};	
-	
-	output.write("Filesize ,  " + dataToWrite);
-	output.newLine();
-	output.close();
-		System.out.println("Transfer started");
-		for(String file: FILENAME){
-			String command = GSUTILPATH +" cp " + BASEPATH + file + " gs://iot-test_hda ";
+		BufferedWriter output;
+		output = new BufferedWriter(new FileWriter(ResultsFileName, false));
+		StringBuilder dataToWrite = new StringBuilder();
+		for (int i : FILEAMOUNT) {
+			dataToWrite.append(i + " Threads , ");
+		}
+		output.write("Filesize , " + dataToWrite.toString());
+		output.newLine();
+		output.close();
 			
-			dataToWrite = "";
-			long tmpElapsedTime = 0;
-			for(int i: FILEAMOUNT){
-				System.out.println("Transferring " + file + " " + i + " times");
-				for(int j=0;j < i;j++){
-					/*
-					 * 
-					 *  Noch nicht lauffähig aber von der idee her halt
-					 * 
-					 * 
-					 */
-
-					Thread r = new Szenario3(command);
-					new Thread(r).start();
-					/*
-					 * 
-					 * 
-					 * HIER MÜSSEN THREADS DANN HIN
-					 * 
-					 * 
-					 * 
-					 * 
-					 * 
-					 */
-					tmpElapsedTime+= interactor.elapsedTime;
-				}
-				dataToWrite += tmpElapsedTime + " , ";
-				System.out.println("Transferred " + file + " " + i +  " times  in " +  tmpElapsedTime + " Ms (" + interactor.elapsedTime/1000 +" sec.)");
-			}
-			
-	
+		for (String nameVal : FILENAME) {
+			// start all threads
 			output = new BufferedWriter(new FileWriter(ResultsFileName, true));
+			output.write(nameVal + ", ");
+			output.close();
+			for (int amountVal : FILEAMOUNT) {
+				String command = GSUTILPATH + " cp " + BASEPATH + nameVal + " gs://iot-test_hda ";
 
-			output.append(file +", " +  dataToWrite);
+				Vector<Long> returnValues = new Vector<>();
+				for (int j = 0; j < amountVal; j++) {
+					Thread r = new Szenario3(/* j, FILENAME[fileSize], */ command, returnValues);
+					new Thread(r).start();
+				}
+
+				// Wait for Returnvalue from the threads
+				while (returnValues.size() < amountVal) {
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				System.out.println(nameVal + " , " + amountVal + " times complete");
+
+				output = new BufferedWriter(new FileWriter(ResultsFileName, true));
+				long max = 0;
+				for (long val : returnValues) {
+					if (val > max) {
+						max = val;
+					}
+				}
+				output.write(max + ",");
+				output.close();
+			}
+			output = new BufferedWriter(new FileWriter(ResultsFileName, true));
 			output.newLine();
 			output.close();
 		}
-		System.out.println("Operation terminated as expected");
+
 	}
+
 }
